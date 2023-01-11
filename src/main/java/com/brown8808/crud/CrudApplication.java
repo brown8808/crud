bb902f4cbc79423e4517e7b52a2865613d406f6b
brown8808 <phh8808@gmail.com>
2023-01-11 23:53:48 +0900

    first

---
+++ src/main/java/com/brown8808/crud/CrudApplication.java
@@ -0,0 +1,13 @@
+package com.brown8808.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+	}
+
+}
