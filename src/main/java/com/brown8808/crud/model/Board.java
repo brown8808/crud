f37a354424cc961b69cb0e9681a5fd738e24de74
brown8808 <phh8808@gmail.com>
2023-01-13 19:24:32 +0900

    게시판2

--- src/main/java/com/brown8808/crud/model/Board.java
+++ src/main/java/com/brown8808/crud/model/Board.java
@@ -1,2 +1,18 @@
-package com.brown8808.crud.model;public class Board {
+package com.brown8808.crud.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
 }
--- src/main/java/com/brown8808/crud/repository/BoardRepository.java
+++ src/main/java/com/brown8808/crud/repository/BoardRepository.java
@@ -1,2 +1,7 @@
-package com.brown8808.crud.repository;public interface BoardRepository {
+package com.brown8808.crud.repository;
+
+import com.brown8808.crud.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
 }
